72b0ed5e94f8def1a13cf1a7c62f780f4737df6f
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2022-12-23 21:28:50 +0300

    -

---
+++ oop_1_wcfservice/oop_1_wcfservice/oop_1_wcfservice/Hizmetli.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace oop_1_wcfservice
+{
+    public class Hizmetli : Personel
+    {
+        public override void maasHesapla()
+        {
+            base.maasHesapla();
+            if (haftalikCalismaSaati <= 30)
+            {
+                this.maas = maas * 2;
+            } else
+            {
+                this.maas = (maas * 2) + ((haftalikCalismaSaati - 30) * 40);
+            }
+        }
+    }
+}
---
+++ oop_1_wcfservice/oop_1_wcfservice/oop_1_wcfservice/Personel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace oop_1_wcfservice
+{
+    public class Personel
+    {
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public double maas { get; set; }
+        public int haftalikCalismaSaati { get; set; } 
+
+        public virtual void maasHesapla()
+        {
+            maas = 2000;
+        }
+    }
+}
